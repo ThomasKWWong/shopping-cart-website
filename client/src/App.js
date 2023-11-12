8228968d425ae7c297903ffa23f899b5a7158761
Thomas Wong <thomaskwwong7@gmail.com>
2023-11-12 00:36:36 -0500

    registration componeent compelted

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Register from './pages/Register'
+
 
 function App() {
   return (
     <div>
-
+      <BrowserRouter>
+        <Routes>
+          <Route index element =  {<Register/>} />
+          <Route path="*" element = {<Register/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- client/src/components/Registration.js
+++ client/src/components/Registration.js
@@ -1,39 +1,49 @@
 import React, { useState } from 'react';
 
 const Registration = () => {
-    const[username, setUsername] = useState('');
-    const[email, setEmail] = useState('');
-    const[password, setPassword] = useState('');
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    //function connects to backend to register to database
     async function registerUser(event) {
-        event.preventDFefault();
+        event.preventDefault();
         const response = await fetch("/api/register", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: 
-                JSON.stringify({
-                    usermame,
-                    email,
-                    password
-                }),
-            })
+            body: JSON.stringify({
+                username,
+                email,
+                password,
+            }),
+        })
 
         const data = await response.json();
+        
+        //send to login page with successful registration
         if(data.status === 'ok') {
             alert("Successful Registration");
+            window.location.href="/login";
         }
-
-
     }
 
     return (
-        <div>
-            
-        </div>
+            <div>
+                <form onSubmit={registerUser}>
+                    <h1>Register</h1> <br/>
+                    <div>
+                        <label>Username</label> <br/>
+                        <input value= {username} onChange={(e) => setUsername(e.target.value)} placeholder="Type your username"/><br/>
+                        <label>Email</label> <br/>
+                        <input value= {email} onChange={(e) => setEmail(e.target.value)} placeholder="Type your email"/><br/>
+                        <label>Password</label><br/>
+                        <input value= {password} onChange={(e) => setPassword(e.target.value)} placeholder='Type your password'/><br/>
+                    </div>
+                    <button type = "submit">Register</button>
+                </form>
+            </div>
     )
 }
 
-export default Registration
+export default Registration;
